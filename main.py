0198241004720d7e0cb1eb914d99895c3dad39a9
LB-Segole <segolelesego17@gmail.com>
2026-09-16 09:58:24 +0200

    imports + app routing

---
+++ main.py
@@ -0,0 +1,4 @@
+# sgl-threat-api
+# A live REST API that analyses passwords, IP addresses, and file hashes against known-bad signatures and returns a real-time security verdict over HTTP.
+
+# how do we start with this?
